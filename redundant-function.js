fa2cacdda100b1d0736c367cda7538b84bf6d368
Johnshandsable <Jshands@asu.edu>
2020-12-31 12:51:12 -0600

    Completed redundant function.js

---
+++ redundant-function.js
@@ -0,0 +1,11 @@
+/*
+Write a function redundant that takes in a string str and returns a function that returns str.
+
+Your function should return a function, not a string.
+*/
+
+function redundant(str) {
+  return function displayName() {
+		return str;
+	}
+}
